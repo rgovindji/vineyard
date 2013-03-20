f8f169f04db50fbfe84b3337bff8bcbd033a1d54
Kyle Decot <kyle.decot@me.com>
2013-03-20 15:09:54 -0400

    Adds HTTParty

--- lib/divine.rb
+++ lib/divine.rb
@@ -1,5 +1,20 @@
 require "divine/version"
+require "httparty"
 
 module Divine
-  # Your code goes here...
+  def self.authenticate(username = '', password = '')
+    options = {
+      :query => {
+        'username' => username,
+        'password' => password
+      },
+      :headers => {
+        'Device-Token' => 'Divine',
+        'User-Agent' => 'com.vine.iphone/1.01 (unknown, iPhone OS 6.0, iPad, Scale/2.000000) (Divine.js/0.0.1)'
+      }
+    }
+    # puts options
+    puts HTTParty.post 'https://api.vineapp.com/users/authenticate', options
+
+  end
 end
